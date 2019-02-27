4e52b3445ec2bf4142288645fb755e048fc41219
Sally Haefling <sallyhaefling@gmail.com>
2019-02-26 20:27:18 -0700

    Refactor and fix names in sass

--- src/FilteredControls/FilteredControls.js
+++ src/FilteredControls/FilteredControls.js
@@ -2,36 +2,38 @@ import React from 'react';
 
 const FilteredControls = (props) => {
   return (
-<section className='left-box'>
+    <section className='left-box'>
         <article className='left-title'>
-        Which prototype methods do you want to practice?
+          Which prototype methods do you want to practice?
         </article>
-        <p className='left-subtitle'>...click on a method below and your card will display on the right!</p>
+        <p className='left-subtitle'>
+          ...click on a method below and your card will display!
+        </p>
         <article className='button-container'>
-        <button 
-          name='iterator'
-          id='Iteration methods' 
-          onClick={props.iteration} 
-          className={`button iteration ${props.activeButton === 'iterator' ? 'active-button' : ''}`}
-        >Iteration Methods
-        </button>
-        <button 
-          name='mutator'
-          id='Mutator methods' 
-          onClick={props.mutator}
-          className={`button mutator ${props.activeButton === 'mutator' ? 'active-button' : ''}`}
-        >Mutator Methods
-        </button>
-        <button 
-          name='accessor'
-          id='Accessor methods' 
-          onClick={props.accessor} 
-          className={`button accessor ${props.activeButton === 'accessor' ? 'active-button' : ''}`}
-        >Accessor Methods
-        </button>
+          <button 
+            name='iterator'
+            id='Iteration methods' 
+            onClick={props.iteration} 
+            className={`button iteration ${props.activeButton === 'iterator' ? 'active-button' : ''}`}
+          >Iteration Methods
+          </button>
+          <button 
+            name='mutator'
+            id='Mutator methods' 
+            onClick={props.mutator}
+            className={`button mutator ${props.activeButton === 'mutator' ? 'active-button' : ''}`}
+          >Mutator Methods
+          </button>
+          <button 
+            name='accessor'
+            id='Accessor methods' 
+            onClick={props.accessor} 
+            className={`button accessor ${props.activeButton === 'accessor' ? 'active-button' : ''}`}
+          >Accessor Methods
+          </button>
         </article>
         <hr></hr>
-      </section>
+    </section>
   )
 }
 
